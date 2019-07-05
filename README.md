b8775e0f02de84c2a1e24ff317b51de246ec6841
Matt Lintz <mattl@inlandtarp.com>
2019-07-04 20:13:41 -0700

    FIrebase integration
    Register / login functionality
    updated dependencies

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # phaser3-typescript-webpack
-
-
 https://github.com/troyedwardsjr/phaser3-typescript-webpack 
+
+Firebase Branch
+Added Menu scene
+
+Added Firebase registration / login
+
+Zondicons - user.svg
+http://www.zondicons.com/
--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,29 @@
 import 'phaser';
+import firebase = require("firebase/app");
+import 'firebase/auth';
+import firebaseConfig from './firebaseConfig';
+import Menu from './scenes/Menu';
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  parent: 'content',
+  dom: { 
+    createContainer: true
+  },
+  width: window.innerWidth,
+  height: window.innerHeight,
 
-const config:Phaser.Types.Core.GameConfig = {
-    type: Phaser.AUTO,
-    parent: 'content',
-    width: window.innerWidth,
-    height: window.innerHeight,
-    resolution: 1, 
-    scale: {
-      mode: Phaser.Scale.RESIZE,
-      autoCenter: Phaser.Scale.CENTER_BOTH
-    },
+  resolution: 1,
+  scale: {
+    mode: Phaser.Scale.RESIZE,
+    autoCenter: Phaser.Scale.CENTER_BOTH
+  },
 
-    backgroundColor: "#000000",
-    scene: [
-      
-    ]
+  backgroundColor: "#000000",
+  scene: [Menu]
 };
 //sclae:mode - https://rexrainbow.github.io/phaser3-rex-notes/docs/site/scalemanager/
 
-let game:Phaser.Game = new Phaser.Game(config);
+let game: Phaser.Game = new Phaser.Game(config);
